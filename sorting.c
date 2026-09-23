77a71e4b35fcc3e86b5bed2a4ab4b38219862f1a
K0KR3 <cacepolojorge@gmail.com>
2026-09-23 19:52:52 +0200

    Add files via upload

--- sorting.c
+++ sorting.c
@@ -9,30 +9,55 @@
  *
  */
 
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "sorting.h"
 
-/***************************************************/
-/* Function: InsertSort    Date:                   */
-/* Your comment                                    */
-/***************************************************/
-int InsertSort(int* array, int ip, int iu)
-{
-  /* Your code */
+/*******************************************************/
+/* Function: InsertSort    Date:23-09-26               */
+/* Your comment                                        */
+/*******************************************************/
+int InsertSort(int* array, int ip, int iu) {
+
+  int i, A, j;
+
+  for(i = ip + 1; i < iu; i++){
+      A = array[i];
+      j = i-1;
+      while (j >= ip && array[j] > A){
+        array[j + 1] = array[j];
+        j--;
+      }
+      array[j+1] = A;
+  }
+
+  return *array;
 }
 
 
 /***************************************************/
-/* Function: SelectSort    Date:                   */
+/* Function: SelectSort    Date:23-09-26           */
 /* Your comment                                    */
 /***************************************************/
 int BubbleSort(int* array, int ip, int iu)
 {
-  /* Your code */
+  
+  int flag = 1, i = iu;
+  int aux = 0, j;
+  
+  while(flag == 1 && i >= ip + 1){
+      flag = 0;
+      for(j = ip; j < i - 1; j++){
+        if(array[j] > array[j + 1]){
+          aux = array[j];
+          array[j + 1]= array[j]; 
+          array[j] = aux;
+          flag = 1;
+        }
+      }
+      i--;
+  }
+
+  return *array;
 }
-
-
-
-
-
-
